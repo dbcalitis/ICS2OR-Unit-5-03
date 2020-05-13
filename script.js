16dd04732008fb549b659c456327a5a66e9d3211
Ubuntu <ubuntu@ip-172-31-88-46.ec2.internal>
2020-05-13 23:39:06 +0000

    Added images, index.html, script.js and style.css

---
+++ script.js
@@ -0,0 +1,18 @@
+// JavaScript File
+let age;
+      document.getElementById("enter").addEventListener("click", ratingTeller);
+      function ratingTeller() {
+        age = document.getElementById("inputbox").value;
+        if (age >= 17) {
+          document.getElementById("response").textContent = "You can watch an R movie alone.";
+        }
+        else if (age >= 13) {
+          document.getElementById("response").textContent = "You can watch a PG-13 movie alone.";
+        }
+        else if (age >= 5) {
+          document.getElementById("response").textContent = "You can watcch a G or PG movie alone.";
+        }
+        else {
+         document.getElementById("response").textContent = "You are too young for these things...";
+        }
+      }
